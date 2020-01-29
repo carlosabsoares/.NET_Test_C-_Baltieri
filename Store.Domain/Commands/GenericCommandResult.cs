9f78b77e63768c1a0d6073d29848c9634c9e5271
carlos alberto baptista soares <carlosabsoares@gmail.com.br>
2020-01-29 18:01:10 -0300

    .

---
+++ Store.Domain/Commands/GenericCommandResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Store.Domain.Commands.Interfaces;
+
+namespace Store.Domain.Commands
+{
+    public class GenericCommandResult: ICommandResult
+    {
+
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+
+        public GenericCommandResult(bool success, string message, object data)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+
+    }
+}
